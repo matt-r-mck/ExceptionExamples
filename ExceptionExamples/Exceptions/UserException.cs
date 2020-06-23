497375f54f4e10ef4708b7f07cee101d7ae92e21
matt-r-mck <matt.r.mck@gmail.com>
2020-06-23 15:40:47 -0400

    added user exceptions

---
+++ ExceptionExamples/Exceptions/UserException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionExamples.Exceptions { //.foldername
+    public class UserException : Exception {//must inherrit from Exception
+
+        public double NumberInError { get; set; } //property to collect number error
+
+        public UserException() : base() { } //override default constructor
+
+        public UserException(string message) : base(message) { } //constructor with message
+
+        public UserException (string message, Exception innerException) 
+            : base(message, innerException) { }//constructor with inner exception and message
+
+    }
+}
--- ExceptionExamples/Program.cs
+++ ExceptionExamples/Program.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using ExceptionExamples.Exceptions;
+using System;
 
 namespace ExceptionExamples {
     class Program {
+
         static void Main(string[] args) {
+            try {
             Run();
+            } catch (UserException ex) {
+                Console.WriteLine($"{ex.Message} number is {ex.NumberInError}");
+            }catch (Exception ex) {
+                Console.WriteLine($"{ex.Message}");
+            }
+            
         }
 
         static void Run() {
             Test();
+            var ex = new UserException("This exception was forced by dev: "); //created exception
+                ex.NumberInError = 42;
+            throw ex;
         }
 
         static void Test() {
@@ -15,9 +27,16 @@ namespace ExceptionExamples {
         }
 
         static void Divide() {
-            var num = 1;
-            var denom = 0;
-            var result = num / denom;
+            try { //try this code
+                var num = 1;
+                var denom = 0;
+                var result = num / denom;
+            } catch (DivideByZeroException ex) { //catch exception named "" as var ex
+                Console.WriteLine($"Exception Occurred: {ex.Message}");
+                //throw;
+            } catch (Exception ex) { //catch any exception
+                Console.WriteLine($"General exception occurred: {ex.Message}");
+            }
         }
 
     }
