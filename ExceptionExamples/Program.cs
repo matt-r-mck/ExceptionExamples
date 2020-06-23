ad1560f891c99728943960345c35ba6b7a1e0005
matt-r-mck <matt.r.mck@gmail.com>
2020-06-23 14:32:32 -0400

    Add project files.

---
+++ ExceptionExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExceptionExamples {
+    class Program {
+        static void Main(string[] args) {
+            Run();
+        }
+
+        static void Run() {
+            Test();
+        }
+
+        static void Test() {
+            Divide();
+        }
+
+        static void Divide() {
+            var num = 1;
+            var denom = 0;
+            var result = num / denom;
+        }
+
+    }
+}
